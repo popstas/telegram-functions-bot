11811b49230c761ba69f2cce9c79bbcbad7abdf7
p0pstas <123875555+p0pstas@users.noreply.github.com>
2024-11-12 22:02:27 +0500

    Import `app`, `Tray`, and `Menu` from `electron` in `src/index.ts`

--- src/index.ts
+++ src/index.ts
@@ -27,7 +27,7 @@ import {readGoogleSheet} from "./helpers/readGoogleSheet.ts";
 import {OAuth2Client} from "google-auth-library/build/src/auth/oauth2client";
 import {GoogleAuth} from "google-auth-library";
 import express from 'express';
-import { app, BrowserWindow, Tray, Menu } from 'electron';
+import { app, Tray, Menu } from 'electron';
 import path from 'path';
 
 export const threads = {} as { [key: number]: ThreadStateType }
