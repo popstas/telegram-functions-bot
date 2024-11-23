f790b3ce1b26c883db7b5d84b3444e7a34bca68c
Stanislav Popov <popstas@gmail.com>
2024-11-24 02:12:00 +0500

    chore: install jest

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,11 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} **/
 export default {
+  preset: 'ts-jest/presets/default-esm',
   testEnvironment: "node",
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  extensionsToTreatAsEsm: ['.ts'],
+  testMatch: ['**/__tests__/**/*.+(ts|tsx|js)', '**/?(*.)+(spec|test).+(ts|tsx|js)'],
   transform: {
-    "^.+.tsx?$": ["ts-jest",{}],
+    // '^.+\\.(ts|tsx)$': 'ts-jest',
   },
 };
