8b223507e6878b0b8b01c7623c012b6c4b5f6530
Stanislav Popov <popstas@gmail.com>
2024-11-24 03:12:50 +0500

    fix: speedup tests
    https://stackoverflow.com/posts/60905543/revisions

--- jest.config.js
+++ jest.config.js
@@ -6,6 +6,6 @@ export default {
   extensionsToTreatAsEsm: ['.ts'],
   testMatch: ['**/__tests__/**/*.+(ts|tsx|js)', '**/?(*.)+(spec|test).+(ts|tsx|js)'],
   transform: {
-    // '^.+\\.(ts|tsx)$': 'ts-jest',
+    '^.+\\.(ts|tsx)$': ['ts-jest', { isolatedModules: true }],
   },
 };
